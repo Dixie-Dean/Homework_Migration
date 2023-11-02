7933305bb3af1340d4034da9ea603c40ca3e47a2
Dixie Dean <d.w.sokolov26@gmail.com>
2023-11-02 13:56:37 +0300

    Initial Commit

---
+++ src/test/java/com/example/homework_migration/HomeworkMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework_migration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkMigrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
